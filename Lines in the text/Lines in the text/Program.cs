ac1d1285d98648ff185317cf0984e7cf4b80a300
GlushenkoBorys <borisss@list.ru>
2019-11-14 21:26:04 +0200

    Work with text

--- Lines in the text/Lines in the text/Program.cs
+++ Lines in the text/Lines in the text/Program.cs
@@ -12,6 +12,15 @@ namespace Lines_in_the_text
     {
         static void Main(string[] args)
         {
+            string text = "He said the government would accelerate the building of structural defences\r\n
+                fo the lagoon city, referring specifically to the so-called Mose project - a hydraulic\r\n
+                barrier system to shut off the lagoon in the event of rising sea levels and winter storms.
+                The prime minister said he was declaring emergency measures on Thursday,
+                 adding that individuals could claim up to €5,000(£4, 300; $5,500), and businesses up to €20,000, in compensation.
+                 Many museums remained shut on Thursday, AFP reports.";
+
+            string my = "hrthetyjrtj getyjetyjhyds thwethrth fgreht5t5h tsrtheth uyiuyiot7io
+            dghtgjhgjh rtyhtht thrtht ";
         }
     }
 }
