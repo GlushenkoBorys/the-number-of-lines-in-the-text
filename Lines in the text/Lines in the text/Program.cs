e090357806c67d85f5411578617827434bbf3750
GlushenkoBorys <borisss@list.ru>
2019-11-16 16:25:44 +0200

    Doing the program

--- Lines in the text/Lines in the text/Program.cs
+++ Lines in the text/Lines in the text/Program.cs
@@ -13,7 +13,7 @@ namespace Lines_in_the_text
         static void Main(string[] args)
         {
             string text = "He said the government would accelerate the building of structural defences\r\n
-                fo the lagoon city, referring specifically to the so-called Mose project - a hydraulic\r\n
+                for the lagoon city, referring specifically to the so-called Mose project - a hydraulic\r\n
                 barrier system to shut off the lagoon in the event of rising sea levels and winter storms.
                 The prime minister said he was declaring emergency measures on Thursday,
                  adding that individuals could claim up to €5,000(£4, 300; $5,500), and businesses up to €20,000, in compensation.
