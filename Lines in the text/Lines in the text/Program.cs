906898b836138f96ef96e156c437797297171b12
GlushenkoBorys <borisss@list.ru>
2019-11-18 00:06:50 +0200

    Use method "ToCharArray"

--- Lines in the text/Lines in the text/Program.cs
+++ Lines in the text/Lines in the text/Program.cs
@@ -14,6 +14,16 @@ namespace Lines_in_the_text
         {
             string text = "He said the government would accelerate the building of structural defences\nfor the lagoon city, referring specifically to the so-called Mose project - a hydraulic\nbarrier system to shut off the lagoon in the event of rising sea levels and winter storms.";
             bool myline = text.Contains("\n");
+            char[] line = text.ToCharArray();
+            int j = 0;
+            for(int i = 0; i < line.Length; i++)
+            {
+                if(line[i] == "\\")
+                {
+                    j++;
+                }                                    
+            }
+            Console.WriteLine(j);
 
             Console.WriteLine(myline);
             Console.WriteLine(text);
