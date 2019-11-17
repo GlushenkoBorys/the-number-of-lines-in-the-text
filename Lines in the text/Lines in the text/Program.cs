7a9ea676097ed6e02253995b838225c95214105f
GlushenkoBorys <borisss@list.ru>
2019-11-17 23:48:01 +0200

    Doing the program

--- Lines in the text/Lines in the text/Program.cs
+++ Lines in the text/Lines in the text/Program.cs
@@ -12,15 +12,9 @@ namespace Lines_in_the_text
     {
         static void Main(string[] args)
         {
-            string text = "He said the government would accelerate the building of structural defences\r\n
-                for the lagoon city, referring specifically to the so-called Mose project - a hydraulic\r\n
-                barrier system to shut off the lagoon in the event of rising sea levels and winter storms.
-                The prime minister said he was declaring emergency measures on Thursday,
-                 adding that individuals could claim up to €5,000(£4, 300; $5,500), and businesses up to €20,000, in compensation.
-                 Many museums remained shut on Thursday, AFP reports.";
-
-            string my = "hrthetyjrtj getyjetyjhyds thwethrth fgreht5t5h tsrtheth uyiuyiot7io
-            dghtgjhgjh rtyhtht thrtht ";
+            string text = "He said the government would accelerate the building of structural defences\nfor the lagoon city, referring specifically to the so-called Mose project - a hydraulic\nbarrier system to shut off the lagoon in the event of rising sea levels and winter storms.";
+            Console.WriteLine(text);
+            Console.WriteLine(text);
         }
     }
 }
