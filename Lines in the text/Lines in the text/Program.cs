f1b316d2792e82e1b2f7d8f4af086f478a81d402
GlushenkoBorys <borisss@list.ru>
2019-11-19 20:01:25 +0200

    I did it!

--- Lines in the text/Lines in the text/Program.cs
+++ Lines in the text/Lines in the text/Program.cs
@@ -12,20 +12,18 @@ namespace Lines_in_the_text
     {
         static void Main(string[] args)
         {
-            string text = "He said the government would accelerate the building of structural defences\nfor the lagoon city, referring specifically to the so-called\nMose project - a hydraulic\nbarrier system to shut off the lagoon in the event of rising sea levels and winter storms.";
+            string text = "He said the government would of structural defences\nfor the lagoon city, referring to the so-called\nMose project - a hydraulic\nbarrier system to shut of rising sea levels and winter storms.";
             bool myline = text.Contains("\n");
             char[] line = text.ToCharArray();
-            int j = 0;
+            int j = 1;
             for(int i = 0; i < line.Length; i++)
             {
-                if(line[i] == '\\')
+                if(line[i] == '\n')
                 {
                     j++;
-                } 
-                                                   
+                }                                  
             }
-            Console.WriteLine(j);
-
+            Console.WriteLine($"Количество предложений в тексте = {j}");
             Console.WriteLine(myline);
             Console.WriteLine(text);
         }
