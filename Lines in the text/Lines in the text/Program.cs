899249c16cd4cc2879364c2fade2a8adbe996fe0
GlushenkoBorys <borisss@list.ru>
2019-11-19 19:41:57 +0200

    Doing the program

--- Lines in the text/Lines in the text/Program.cs
+++ Lines in the text/Lines in the text/Program.cs
@@ -12,16 +12,17 @@ namespace Lines_in_the_text
     {
         static void Main(string[] args)
         {
-            string text = "He said the government would accelerate the building of structural defences\nfor the lagoon city, referring specifically to the so-called Mose project - a hydraulic\nbarrier system to shut off the lagoon in the event of rising sea levels and winter storms.";
+            string text = "He said the government would accelerate the building of structural defences\nfor the lagoon city, referring specifically to the so-called\nMose project - a hydraulic\nbarrier system to shut off the lagoon in the event of rising sea levels and winter storms.";
             bool myline = text.Contains("\n");
             char[] line = text.ToCharArray();
             int j = 0;
             for(int i = 0; i < line.Length; i++)
             {
-                if(line[i] == "\\")
+                if(line[i] == '\\')
                 {
                     j++;
-                }                                    
+                } 
+                                                   
             }
             Console.WriteLine(j);
 
