bc297321a5e8be6db7073ce6313c13058ffa4a3d
GlushenkoBorys <borisss@list.ru>
2019-11-22 18:03:53 +0200

    New version of the program

--- Lines in the text/Lines in the text/Program.cs
+++ Lines in the text/Lines in the text/Program.cs
@@ -15,15 +15,49 @@ namespace Lines_in_the_text
             string text = "He said the government would of structural defences\nfor the lagoon city, referring to the so-called\nMose project - a hydraulic\nbarrier system to shut of rising sea levels and winter storms.";
             bool myline = text.Contains("\n");
             char[] line = text.ToCharArray();
-            int j = 1;
-            for(int i = 0; i < line.Length; i++)
+            int numberLines = 1;
+            int numberletters = 0;
+            for (int i = 0; i < line.Length; i++)
             {
                 if(line[i] == '\n')
                 {
-                    j++;
-                }                                  
+                    numberLines++;
+                }  
+                
+                if(line[i] != ' ')
+                {
+                    if(line[i] != '.')
+                    {
+                        if (line[i] != ',')
+                        {
+                            if (line[i] != '"')
+                            {
+                                if (line[i] != '!')
+                                {
+                                    if (line[i] != '?')
+                                    {
+                                        if (line[i] != ':')
+                                        {
+                                            if (line[i] != ';')
+                                            {
+                                                if (line[i] != '\n')
+                                                {
+                                                    if (line[i] != '-')
+                                                    {
+                                                        numberletters++;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }                              
             }
-            Console.WriteLine($"Количество предложений в тексте = {j}");
+            Console.WriteLine($"Количество строк в тексте = {numberLines}");
+            Console.WriteLine($"Количество букв в тексте = {numberletters}");
             Console.WriteLine(myline);
             Console.WriteLine(text);
         }
