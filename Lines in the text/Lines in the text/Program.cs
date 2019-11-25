bba1efd47acbdbd7649b622ebea50838a13e892a
GlushenkoBorys <borisss@list.ru>
2019-11-25 23:27:38 +0200

    Method №3

--- Lines in the text/Lines in the text/Program.cs
+++ Lines in the text/Lines in the text/Program.cs
@@ -28,7 +28,8 @@ namespace Lines_in_the_text
                     numberLines++;
                 }
 
-                if (line[i] != ' ')
+                // method 1
+                if (line[i] != ' ') 
                 {
                     if (line[i] != '.')
                     {
@@ -60,7 +61,8 @@ namespace Lines_in_the_text
                     }
                 }
 
-                if (line[i] != ' ' &
+                // method 2
+                if (line[i] != ' ' & 
                     line[i] != '.' &
                     line[i] != ',' &
                     line[i] != '"' &
@@ -74,11 +76,20 @@ namespace Lines_in_the_text
                     numberletters2++;
                 }
 
-
+                //method 3
+                if (line[i] > 64 & line[i] < 91) 
+                {
+                    numberletters3++;
+                }
+                if(line[i] > 96 & line[i] < 123)
+                {
+                    numberletters3++;
+                }
             }
             Console.WriteLine($"Количество строк в тексте = {numberLines}");
             Console.WriteLine($"Количество букв в тексте (метод 1) = {numberletters1}");
             Console.WriteLine($"Количество букв в тексте (метод 2) = {numberletters2}");
+            Console.WriteLine($"Количество букв в тексте (метод 3) = {numberletters3}");
             Console.WriteLine(mylineIsTrue);
             Console.WriteLine(text);
         }
